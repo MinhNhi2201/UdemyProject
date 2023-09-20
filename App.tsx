ef9151456371ce59f00da0d4263fe57aa949ae87
Nhi, Pham Minh <nhipmz@nec.vn>
2023-09-20 17:30:21 +0700

    done -14

--- App.tsx
+++ App.tsx
@@ -2,13 +2,20 @@ import React, {useState} from 'react';
 import Routes from './Routes';
 
 export const UserContext = React.createContext();
+export const ProfileContext = React.createContext();
+export const ServiceContext = React.createContext();
 
 function App(): JSX.Element {
   const [user, setUser] = useState();
-
+  const [profile, setProfile] = useState();
+  const [services, setServices] = useState([]);
   return (
     <UserContext.Provider value={{user, setUser}}>
-      <Routes />
+      <ProfileContext.Provider value={{profile, setProfile}}>
+        <ServiceContext.Provider value={{services, setServices}}>
+          <Routes />
+        </ServiceContext.Provider>
+      </ProfileContext.Provider>
     </UserContext.Provider>
   );
 }
